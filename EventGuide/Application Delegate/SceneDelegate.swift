5759858d5fa9d92b831c4ecfb20caed55941d073
Anup Deshpande <deshpande.anup24@gmail.com>
2021-02-04 16:08:16 -0500

    fix: rows in event list
    - Fixed Image size
    - Set label to show only one line
    - Set font to match UIKit implementation

--- EventGuide/Application Delegate/SceneDelegate.swift
+++ EventGuide/Application Delegate/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Create the SwiftUI view that provides the window contents.
-        let mainView = MainView()
+        let mainView = EventListView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
---
+++ EventGuide/EventListView.swift
@@ -0,0 +1,69 @@
+//
+//  MainView.swift
+//  EventGuide
+//
+//  Created by Anup Deshpande on 2/3/21.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct EventListView: View {
+    
+    @State private var events = [Event]()
+    
+    var body: some View {
+        NavigationView{
+            List(events, id: \.id){ event in
+                HStack{
+                    
+                    KFImage.url(URL(string: event.performers.first?.image ?? "")!)
+                        .resizable()
+                        .placeholder{
+                            Image(systemName: "photo.on.rectangle")
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                        }
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 60, height: 60)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    VStack(alignment: .leading){
+                        Text(event.title)
+                            .font(.system(size: 17))
+                            .fontWeight(.medium)
+                            .lineLimit(1)
+                        
+                        Text(event.dateTime)
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    
+                }
+            }
+            .onAppear(perform: fetchEvents)
+            .navigationBarTitle("Event Guide", displayMode: .inline)
+        }
+    }
+    
+    func fetchEvents(){
+        // Refresh events from the API
+        NetworkService.request(endpoint: .getEvents) { (result: Result< EventList, Error>) in
+            switch result{
+            case .success(let eventList):
+                DispatchQueue.main.async {
+                    events = eventList.events
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventListView()
+    }
+}
